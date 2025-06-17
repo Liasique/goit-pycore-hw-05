73afdea76c957a8c37aa3ea6da3201356e5666ff
Ollie <olgalukianets7@gmail.com>
2025-06-17 15:43:05 +0200

    task1 done

---
+++ task1/fibonacci.py
@@ -0,0 +1,42 @@
+#TODO: Create function caching_fibonacci() that returns an inner function fibonacci(n) with caching 
+
+#PSEUDOCODE:
+"""
+ФУНКЦІЯ caching_fibonacci
+    Створити порожній словник cache
+
+    ФУНКЦІЯ fibonacci(n)
+        Якщо n <= 0, повернути 0
+        Якщо n == 1, повернути 1
+        Якщо n у cache, повернути cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        Повернути cache[n]
+
+    Повернути функцію fibonacci
+КІНЕЦЬ ФУНКЦІЇ caching_fibonacci
+"""
+
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
+print(fib(20))  # Виведе 6765
